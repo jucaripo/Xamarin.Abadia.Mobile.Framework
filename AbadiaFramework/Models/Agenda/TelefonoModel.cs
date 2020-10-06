c18ecdc69dfdde75d3ca0a5f6bab8cb36ee431ec
jucaripo <jucaripo@hotmail.com>
2020-10-06 12:30:14 -0500

    * TelefonoModel.cs:
    * AbadiaFramework.csproj: Agenda modulo v1

---
+++ AbadiaFramework/Models/Agenda/TelefonoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AbadiaFramework.Models.Agenda
+{
+    public class TelefonoModel
+    {
+        public int id { get; set; }
+        public string numero {get; set; }
+        public string tipo { get; set; } // Celular, Fino
+        public string lada { get; set; } // clave lada
+
+
+
+
+        public TelefonoModel()
+        {
+        }
+    }
+}
